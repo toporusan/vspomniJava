4566828e7af726c0f42bbed3c3462de792b844ed
vasif_s <cdm_89@mail.ru>
2026-02-17 11:33:12 +0500

    17.02.2026

--- src/test/java/Day1/HTTPRequests.java
+++ src/test/java/Day1/HTTPRequests.java
@@ -18,7 +18,7 @@ public class HTTPRequests {
                 .statusCode(200)
                 .log().all();
 
-        System.out.println("sad-----sd");
+        System.out.println("seeee--sd");
 
     }
 }
