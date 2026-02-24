6bdc03659c02b341cb417b1712a26c60bfa628c7
vasif_s <cdm_89@mail.ru>
2026-02-24 14:36:19 +0500

    24.02.2026

--- src/test/java/Day4/ParsingJsonResponseData.java
+++ src/test/java/Day4/ParsingJsonResponseData.java
@@ -1,6 +1,13 @@
 package Day4;
 
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,8 +23,35 @@ public class ParsingJsonResponseData {
                 .then()
                 .statusCode(200)
                 .header("Content-Type", "application/json")
-                .body("book[1].category", equalTo("fiction"))
-                .log().all();
+                .body("book[4].category", equalTo("dystopian"));
+
+    }
+
+    @Test(priority = 2)
+    public void testJsonResponse2() {
+        Response res = given()
+                .contentType("application/json")
+                .when()
+                .get("http://localhost:3000/store");
+
+        Assert.assertEquals(res.statusCode(), 200); //  валидация статус кода
+        System.out.println("statusCode: " + res.statusCode());
+        Assert.assertEquals(res.getHeader("Content-Type"), "application/json"); // Валидация хедера
+        System.out.println("header: " + res.getHeader("Content-Type"));
+        Assert.assertEquals(res.jsonPath().get("book[3].title").toString(), "The Lord of the Rings"); // валидация поля json
+        System.out.println("title: " + res.jsonPath().get("book[3].title"));
+
+        JSONObject jo = new JSONObject(res.asString());
+        JSONArray books = jo.getJSONArray("book");
+
+        List<String> titles = new ArrayList<>();
+        books.forEach(book -> {
+            JSONObject bookObj = (JSONObject) book;
+            titles.add(bookObj.getString("title"));
+        });
+
+        System.out.println(titles);
+
 
     }
 }
