acbde278edfe5c19b31a30bb3003067be222a805
vasif_s <cdm_89@mail.ru>
2026-03-31 18:35:03 +0500

    31.03.2026

---
+++ .idea/fileTemplates/code/JUnit5 Test Class.java
@@ -0,0 +1,7 @@
+i  import org.junit.jupiter.api.Test;                                                                                                                                                                                                    
+  import static org.assertj.core.api.Assertions.*;                                                                                                                                                                                      
+                                                                                                                                                                                                                                        
+  #parse("File Header.java")                                                                                                                                                                                                            
+  class ${NAME} {                                                                                                                                                                                                                       
+    ${BODY}                                                                                                                                                                                                                             
+  }  
